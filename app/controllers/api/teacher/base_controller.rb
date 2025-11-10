6874faf3515d17874b10fbfcdca8cb6909292dd1
KappaChungus <janek.code.master@gmail.com>
2025-11-10 15:00:47 +0100

    refactor teacher frontend make it use utils and upi remove duplicated code

--- app/controllers/api/teacher/base_controller.rb
+++ app/controllers/api/teacher/base_controller.rb
@@ -10,7 +10,6 @@ module Api
         render json: { error: "Access denied" }, status: :forbidden unless current_user.is_a?(::Teacher)
       end
 
-      # Helper method to get current teacher
       def current_teacher
         @current_teacher ||= current_user.becomes(::Teacher)
       end
